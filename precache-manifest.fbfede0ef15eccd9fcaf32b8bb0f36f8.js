a087291e2cb19fea7292d1fda9b1ca42108ed862
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-08-20 08:35:04 -0700

    Updates

---
+++ precache-manifest.fbfede0ef15eccd9fcaf32b8bb0f36f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fb9da87d3a9e40753bfb54936ec65d",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "fa47b5cfb3af19586fc0",
+    "url": "/Netflix/static/css/main.b01461d0.chunk.css"
+  },
+  {
+    "revision": "c0d4fea6532edbab2245",
+    "url": "/Netflix/static/js/2.e3b9a008.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix/static/js/2.e3b9a008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa47b5cfb3af19586fc0",
+    "url": "/Netflix/static/js/main.3b500b61.chunk.js"
+  },
+  {
+    "revision": "e4594d058fc7eb217f5c",
+    "url": "/Netflix/static/js/runtime-main.56e2df7b.js"
+  },
+  {
+    "revision": "2ea85d7448475a744c1485c2eac3d3d1",
+    "url": "/Netflix/static/media/logo.2ea85d74.png"
+  }
+]);
